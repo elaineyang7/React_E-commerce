ba3653aa0e07a9135229365628eabbd9cd05a0c8
elaineapp082020 <joyfulelaine7@gmail.com>
2020-12-16 17:54:58 -0800

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,42 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBqN_5agjy59WnHJTXlQ8_qvcWkShLI7e4",
-    authDomain: "lamb-db.firebaseapp.com",
-    databaseURL: "https://lamb-db.firebaseio.com",
-    projectId: "lamb-db",
-    storageBucket: "lamb-db.appspot.com",
-    messagingSenderId: "273004016121",
-    appId: "1:273004016121:web:8ea814147d128190858d28"
+  apiKey: "AIzaSyBqN_5agjy59WnHJTXlQ8_qvcWkShLI7e4",
+  authDomain: "lamb-db.firebaseapp.com",
+  databaseURL: "https://lamb-db.firebaseio.com",
+  projectId: "lamb-db",
+  storageBucket: "lamb-db.appspot.com",
+  messagingSenderId: "273004016121",
+  appId: "1:273004016121:web:8ea814147d128190858d28"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
